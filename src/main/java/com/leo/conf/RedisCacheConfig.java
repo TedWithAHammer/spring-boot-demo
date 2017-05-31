94916994cff0c14ba492bcffd972934f917a1038
wangliying <wangliyingwad@163.com>
2017-05-31 23:52:03 +0800

    2017／5／31 deploy on tomcat

---
+++ src/main/java/com/leo/conf/RedisCacheConfig.java
@@ -0,0 +1,45 @@
+package com.leo.conf;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by wangliying on 2017/4/18.
+ * Description:redis configuration
+ */
+@Configuration
+@EnableCaching
+public class RedisCacheConfig extends CachingConfigurerSupport{
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object o, Method method, Object... objects) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(o.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : objects) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+
+        return super.cacheManager();
+    }
+
+//    public JedisConnectionFactory redisConnectorFactory
+}
